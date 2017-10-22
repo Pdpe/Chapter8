51c9c18ba921758b3bf87d0aa29ada1d6d761cac
pdp <pdp@pdpc.local>
2017-10-22 19:43:27 -0400

    Extra credit changes

--- app/app.py
+++ app/app.py
@@ -8,20 +8,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 
-    response        = urllib2.urlopen('https://api.cryptonator.com/api/full/btc-usd')
+    response  = urllib2.urlopen('http://api.fixer.io/latest?base=USD')
     data  = json.load(response)
 
-    ticker_data         = data["ticker"]
-    timestamp           = data["timestamp"]
-    btc_markets_records = []
 
-    for market_record in ticker_data["markets"]:
-        record_tuple = (timestamp, market_record["market"], market_record["price"], market_record["volume"])
-        btc_markets_records.append(record_tuple)
+    timestamp          = data["date"]
+    fx_markets_records = []
 
-    return render_template('index.html', records=btc_markets_records)
+    for key, value in data["rates"].items():
+            record_tuple = (timestamp, key, value)
+            fx_markets_records.append(record_tuple)
 
+    return render_template('index.html', records=fx_markets_records)
 
 if __name__ == '__main__':
     # Starts the Flask application server
-    app.run(debug=True)
+    app.run(debug=False)
