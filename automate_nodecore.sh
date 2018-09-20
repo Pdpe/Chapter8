582d77287572e554a6a8a7c041de7c5040c901ce
Pierre De <29659459+Pdpe@users.noreply.github.com>
2018-09-19 22:15:49 -0400

    Update automate_nodecore.sh
    ip update

--- automate_nodecore.sh
+++ automate_nodecore.sh
@@ -4,7 +4,7 @@ killall screen
 echo "Initializing miners..."
 for i in `seq 0 7`;
 do
-	screen -dm bash -c "./nodecore_pow_cuda_1 -o 172.16.16.116:8501 -u VBYTXuj2Q8meiNXHmWiEzZWPd2zXSu -d $i; ecex sh;"
+	screen -dm bash -c "./nodecore_pow_cuda_1 -o 74.79.107.169:8501 -u VBYTXuj2Q8meiNXHmWiEzZWPd2zXSu -d $i"
 	echo ... $i done.
 done
 echo "All miners initialized."
